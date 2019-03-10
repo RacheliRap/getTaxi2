d43c3af99733bad0a637e34f0219c5b931f5201b
y00s <yosef@quickode.com>
2019-03-10 11:37:47 +0200

    add splash to manifest

--- app/src/main/java/com/example/racheli/gettaxi2/controller/SplashActivity.java
+++ app/src/main/java/com/example/racheli/gettaxi2/controller/SplashActivity.java
@@ -3,20 +3,19 @@ package com.example.racheli.gettaxi2.controller;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.example.racheli.gettaxi2.R;
 import com.example.racheli.gettaxi2.model.backend.Backend;
 import com.example.racheli.gettaxi2.model.backend.BackendFactory;
-import com.example.racheli.gettaxi2.model.datasource.Firebase_DBManager;
 
 public class SplashActivity extends Activity {
-    Backend instance = BackendFactory.getInstance(getApplicationContext());
+    Backend instance;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        instance = BackendFactory.getInstance(getApplicationContext());
 
         Thread welcomeThread = new Thread() {
 
